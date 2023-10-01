237fb123f1f6e03b2dbf5490b65bc20e0fa83b1b
jonathantho1 <jonathantho@binus.ac.id>
2023-10-01 22:29:48 +0700

    edit Ujian minggu ke-2 (3)

--- src/test/java/com/juaracoding/BankAccountTest.java
+++ src/test/java/com/juaracoding/BankAccountTest.java
@@ -7,27 +7,28 @@ public class BankAccountTest {
 
     BankAccount bankAccount = new BankAccount(10011103,"Jonathantho",10000);
 
-    @Test
+    @Test (priority = 1)
     public void depositTest(){
         Assert.assertEquals(bankAccount.deposit(10000),20000);
     }
 
-    @Test
+    @Test (priority = 2)
     public void withdrawTestLessAmount(){
         Assert.assertEquals(bankAccount.withdraw(10000),10000);
     }
 
-    @Test
+    @Test (priority = 3)
     public void withdrawTestMoreAmount(){
         Assert.assertEquals(bankAccount.withdraw(25000),10000);
     }
 
-    @Test
+    @Test (priority = 4)
     public void checkBalanceTest(){
         Assert.assertEquals(bankAccount.checkBalance(), "Balance is: 10000.0");
     }
-    @Test
+
+    @Test (priority = 5)
     public void displayTest(){
-        Assert.assertEquals(bankAccount.display(), "10011103 Jonathantho 20000.0");
+        Assert.assertEquals(bankAccount.display(), "10011103 Jonathantho 10000.0");
     }
 }
